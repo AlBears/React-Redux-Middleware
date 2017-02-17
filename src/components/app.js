f0d7a0ecd8a3d1a65fb5815901081d01bf472cea
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-02-17 17:13:45 +0300

    users component

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import UsersList from './users_component';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <UsersList/>
     );
   }
 }
